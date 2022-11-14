76d85a605ef2e1d7dcf14528ba554bb3cf83e937
JunHyung1206 <pjh120610@naver.com>
2022-11-14 16:25:01 +0000

    Feat: utils.py 작성, 콜백 함수 및 딕셔너리 관리

---
+++ Utils/utils.py
@@ -0,0 +1,28 @@
+import os
+import Instances.Models.loss as loss
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+
+def early_stop(monitor, patience, mode):
+    early_stop_callback = EarlyStopping(monitor=monitor, min_delta=0.00, patience=patience, verbose=False, mode=mode)
+    return early_stop_callback
+
+
+def best_save(save_path, top_k, monitor, mode, filename):
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=save_path,
+        save_top_k=top_k,
+        monitor=monitor,
+        mode=mode,
+        filename=filename,
+    )
+    return checkpoint_callback
+
+loss_dict = {"l1": loss.L1_loss, "mse": loss.MSE_loss, "bce": loss.BCEWithLogitsLoss, "rmse": loss.RMSE_loss, "huber": loss.HUBER_loss, "ce":loss.CrossEntropyLoss}
+
+monitor_dict = {
+    "val_loss": {"monitor": "val_loss", "mode": "min"},
+    "val_pearson": {"monitor": "val_pearson", "mode": "max"},
+}
