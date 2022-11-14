961d8d332af73c56c414fc2c528b423c84f37bc0
JunHyung1206 <pjh120610@naver.com>
2022-11-14 11:21:37 +0000

    Feat: main.py 작성

---
+++ main.py
@@ -0,0 +1,70 @@
+import argparse
+import random
+import numpy as np
+import torch
+import pytorch_lightning as pl
+
+from omegaconf import OmegaConf
+from Step import train, inference, sweep
+
+
+def init(): # args : 실행시 입력하는 인자, conf : yaml 파일에 저장된 하이퍼파라미터
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", "-m", required=True)
+    parser.add_argument("--config", "-c", type=str, default="base_config")
+
+    parser.add_argument(
+        "--saved_model",
+        "-s",
+        default=None,
+        help="저장된 모델의 파일 경로를 입력해주세요. 예시: save_models/klue/roberta-small/epoch=?-step=?.ckpt 또는 save_models/model.pt",
+    )
+    args, _ = parser.parse_known_args()
+    conf = OmegaConf.load(f"./Config/{args.config}.yaml")
+
+    SEED = conf.utils.seed
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
+    torch.backends.cudnn.benchmark = False
+    torch.use_deterministic_algorithms(True)
+    return args, conf
+
+if __name__ == "__main__":
+
+    args, conf = init()
+
+    if args.mode == "train" or args.mode == "t":
+        if conf.k_fold.use_k_fold:  # num_folds 변수 확인
+            train.k_fold_train(args, conf)
+        else:
+            train.train(args, conf)
+
+    elif args.mode == "continue" or args.mode == "c":
+        if args.saved_model is None:
+            print("경로를 입력해주세요")
+        elif conf.k_fold.use_k_fold:
+            print("K-Fold 추가 학습 불가능")
+        else:
+            train.continue_train(args, conf)
+
+    elif args.mode == "exp" or args.mode == "e":
+        exp_count = int(input("실험할 횟수를 입력해주세요 "))
+        sweep.sweep(args, conf, exp_count)
+
+    elif args.mode == "inference" or args.mode == "i":
+        if args.saved_model is None:
+            print("경로를 입력해주세요")
+        else:
+            if conf.k_fold.use_k_fold:  # num_folds 변수 확인
+                inference.k_fold_inference(args, conf)
+            else:
+                inference.inference(args, conf)
+
+    else:
+        print("모드를 다시 설정해주세요 ")
+        print("train        : t,\ttrain")
+        print("continue     : c,\tcontinue")
+        print("sweep        : e,\texp")
+        print("inference    : i,\tinference")
