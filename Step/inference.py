8eaa8ce8f1ae37183803c17de63a07fa863d0040
JunHyung1206 <pjh120610@naver.com>
2022-11-15 16:19:11 +0000

    Feat: predict 부분 구현 완료

--- Step/inference.py
+++ Step/inference.py
@@ -3,6 +3,8 @@ import pandas as pd
 import torch
 import pytorch_lightning as pl
 import Instances.instance as instance
+import Utils.labels_ids as labels_ids
+import torch.nn.functional as F
 
 
 def inference(args, conf):
@@ -16,5 +18,30 @@ def inference(args, conf):
 
     dataloader, model, args, conf = instance.load_instance(args, conf)
 
-    test_micro_f1 = trainer.test(model=model, datamodule=dataloader)
-    test_micro_f1 = test_micro_f1[0]["test_micro_f1"]
+    trainer.test(model=model, datamodule=dataloader)
+    predictions = trainer.predict(
+        model=model,
+        datamodule=dataloader,
+    )
+
+    # 저번 베이스라인 코드와 이번 베이스라인 코드 결합
+
+    predictions = list(i for i in torch.cat(predictions))  # logits들의 배열
+
+    output_pred = [np.argmax(logit, axis=-1).item() for logit in predictions]
+    output_prob = [F.softmax(logit, dim=-1).tolist() for logit in predictions]
+    pred_answer = labels_ids.num_to_label(output_pred)
+
+    predict_data = pd.read_csv(conf.path.predict_path)
+    predict_id = predict_data["id"]
+    output = pd.DataFrame(
+        {
+            "id": predict_id,
+            "pred_label": pred_answer,
+            "probs": output_prob,
+        }
+    )
+
+    output.to_csv(
+        "./prediction/submission.csv", index=False
+    )  # 최종적으로 완성된 예측한 라벨 csv 파일 형태로 저장.
