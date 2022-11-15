00ca191bb08c231c13f4bdc073bd638fa566c14f
JunHyung1206 <pjh120610@naver.com>
2022-11-15 13:10:11 +0000

    Feat: 콜백함수 추가 및 마지막 모델 저장 추가

--- Step/train.py
+++ Step/train.py
@@ -20,6 +20,27 @@ def train(args, conf):
         max_epochs=conf.train.max_epoch,
         log_every_n_steps=1,
         logger=wandb_logger,
+        callbacks=[
+            utils.early_stop(
+                monitor=utils.monitor_dict[conf.utils.early_stop_monitor]["monitor"],
+                mode=utils.monitor_dict[conf.utils.early_stop_monitor]["mode"],
+                patience=conf.utils.patience,
+            ),
+            utils.best_save(
+                save_path=save_path,
+                top_k=conf.utils.top_k,
+                monitor=utils.monitor_dict[conf.utils.best_save_monitor]["monitor"],
+                mode=utils.monitor_dict[conf.utils.best_save_monitor]["mode"],
+                filename="{epoch}-{val_micro_f1}",
+            ),
+        ],
     )
     trainer.fit(model=model, datamodule=dataloader)
+    test_micro_f1=trainer.test(model=model, datamodule=dataloader)
     wandb.finish()
+    
+    
+    # 마지막 모델을 저장합니다
+    test_micro_f1 = test_micro_f1[0]["test_micro_f1"]
+    trainer.save_checkpoint(f"{save_path}epoch={conf.train.max_epoch-1}-test_micro_f1={test_micro_f1}.ckpt")
+
