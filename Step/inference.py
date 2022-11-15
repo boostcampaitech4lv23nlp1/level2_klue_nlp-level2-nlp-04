b8887d7b1f0c142407b5363e4ded2bd642bcd44c
JunHyung1206 <pjh120610@naver.com>
2022-11-15 13:18:12 +0000

    Feat: inference.py 작성

---
+++ Step/inference.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import torch
+import pytorch_lightning as pl
+import Instances.instance as instance
+
+
+def inference(args, conf):
+
+    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=conf.train.max_epoch, log_every_n_steps=1)
+
+    dataloader, model, args, conf = instance.load_instance(args, conf)
+
+    test_micro_f1 = trainer.test(model=model, datamodule=dataloader)
+    test_micro_f1 = test_micro_f1[0]["test_micro_f1"]
