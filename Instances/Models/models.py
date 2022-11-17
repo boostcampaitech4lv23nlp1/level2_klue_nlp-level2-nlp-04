4c76284a1e979cbca9183709a2979deda4ef70d8
JunHyung1206 <pjh120610@naver.com>
2022-11-17 16:28:03 +0000

    Feat: 모델링 예제 추가

--- Instances/Models/models.py
+++ Instances/Models/models.py
@@ -131,3 +131,139 @@ class Model(pl.LightningModule):
             freeze_list = []
             if name in freeze_list:
                 param.requires_grad = False
+
+
+class ExampleModel1(pl.LightningModule):
+    def __init__(self, conf, new_vocab_size):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.model_name = conf.model.model_name
+        self.lr = conf.train.lr
+        self.model_config = transformers.AutoConfig.from_pretrained(self.model_name)  # classifier의 input 차원을 얻어오기 위해 모델 정보를 불러옵니다
+        self.input_dim = self.model_config.hidden_size  # input 차원입니다(cls토큰의 차원)
+        self.num_labels = 30  # 최종 output label의 개수입니다(차원)
+        # self.input_dim = self.model_config.d_model  # 가끔 다른 모델 구조는 input 차원을 d_model로 사용하기도 합니다입니다(cls토큰의 차원)
+
+        self.plm = transformers.AutoModel.from_pretrained(self.model_name)
+        self.warm_up = conf.train.warm_up
+
+        ## print문 주석 해제해서 임베딩 차원이 어떻게 바뀌는지 한번 확인해보세요
+        # print(self.plm)
+
+        self.plm.resize_token_embeddings(new_vocab_size)  # vocab 사이즈 조정 (새로운 토큰 추가에 의함)
+
+        if self.plm.config.type_vocab_size == 1:  # classifier 모듈이 없어, 감싸져있지 않습니다 따라서 모두 통일된 형태입니다
+            self.plm.config.type_vocab_size = 2
+            single_emb = self.plm.embeddings.token_type_embeddings
+            self.plm.embeddings.token_type_embeddings = torch.nn.Embedding(2, single_emb.embedding_dim)
+            self.plm.embeddings.token_type_embeddings.weight = torch.nn.Parameter(single_emb.weight.repeat([2, 1]))
+
+        # print(self.plm)
+
+        self.loss_func = utils.loss_dict[conf.train.loss]
+        self.use_freeze = conf.train.use_freeze
+
+        if self.use_freeze:
+            self.freeze()
+
+        # classifier를 정의해줍니다, 최종 num_label은 30으로 고정되어야합니다.
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.1),
+            nn.Linear(self.input_dim, self.num_labels),
+        )
+
+    def forward(self, items):  ## **items
+        x = self.plm(
+            input_ids=items["input_ids"],
+            attention_mask=items["attention_mask"],
+            token_type_ids=items["token_type_ids"],
+        )  # pooler까지 거친 최종적인 output입니다
+        x = x.pooler_output  # 위의 값에서 cls 토큰을 받아옵니다
+        x = self.classifier(x)  # 분류기를 거칩니다
+        return x  # 분류기를 거친 최종 30차원 탠서를 반환해줍니다
+
+    def training_step(self, batch, batch_idx):
+        items = batch
+
+        logits = self(items)
+        loss = self.loss_func(logits, items["labels"].long())
+        self.log("train_loss", loss)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        items = batch
+
+        logits = self(items)
+        loss = self.loss_func(logits, items["labels"].long())
+        pred = logits.argmax(-1)  # pred 한 라벨
+
+        return {"val_loss": loss, "pred": pred, "label": items["labels"]}
+
+    def validation_epoch_end(self, outputs):
+        pred_all = torch.concat([x["pred"] for x in outputs])  # 배치당 예측한 라벨들을 전부 concat 하여 전체 예측 텐서 생성
+        label_all = torch.concat([x["label"] for x in outputs])  # 배치당 정답 라벨을 전부 concat하여 전체 정답 텐서 생성
+        val_f1 = metric.klue_re_micro_f1(pred_all.cpu().numpy(), label_all.cpu().numpy())  # micro_f1 계산
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()  # 배치당 loss를 전부 stack으로 받아 평균 취함
+
+        self.log("val_micro_f1", val_f1)
+        self.log("val_loss", avg_loss)
+
+    def test_step(self, batch, batch_idx):
+        items = batch
+
+        logits = self(items)
+        pred = logits.argmax(-1)  # pred 한 라벨
+        return {"pred": pred, "label": items["labels"]}
+
+    def test_epoch_end(self, outputs):
+        pred_all = torch.concat([x["pred"] for x in outputs])  # 배치당 예측한 라벨들을 전부 concat 하여 전체 예측 텐서 생성
+        label_all = torch.concat([x["label"] for x in outputs])  # 배치당 정답 라벨을 전부 concat하여 전체 정답 텐서 생성
+        test_f1 = metric.klue_re_micro_f1(pred_all.cpu().numpy(), label_all.cpu().numpy())  # micro_f1 계산
+
+        self.log("test_micro_f1", test_f1)
+
+    def predict_step(self, batch, batch_idx):
+        items = batch
+        logits = self(items)
+
+        return logits.squeeze()
+
+    ## Warm-up 단계 밑의 링크 참고하여 작성
+    ## https://pytorch-lightning.readthedocs.io/en/stable/common/optimization.html
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)  # 추후에 알려드릴 예정
+        scheduler = LambdaLR(optimizer=optimizer, lr_lambda=lambda step: min(1.0, float(step + 1) / (self.warm_up + 1)))
+        return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
+
+    def freeze(self):
+        for name, param in self.plm.named_parameters():
+            freeze_list = []
+            if name in freeze_list:
+                param.requires_grad = False
+
+
+# 상속을 받으면 기존에 구현되어 있는 코드를 재활용할 수 있습니다
+# 상속받은 자식클래스에 classifier가 없다면 부모클래스에 있던 classifier가 print 할때는 나오지만 forward 과정에서는 사용되지 않습니다
+# 이렇게 안하시고 복붙하셔도 상관없습니다
+class ExampleModel2(ExampleModel1):
+    def __init__(self, conf, new_vocab_size):
+        super().__init__(conf, new_vocab_size)
+
+        self.hidden_dim = 1024
+
+        self.classifier = nn.Sequential(nn.Linear(self.input_dim, self.hidden_dim), nn.ReLU(), nn.Dropout(0.2))
+        self.classifier2 = nn.Sequential(nn.Linear(self.input_dim + self.hidden_dim, self.num_labels))
+
+    def forward(self, items):  ## **items
+        x = self.plm(
+            input_ids=items["input_ids"],
+            attention_mask=items["attention_mask"],
+            token_type_ids=items["token_type_ids"],
+        )  # pooler까지 거친 최종적인 output입니다
+        x = x.pooler_output  # 위의 값에서 cls 토큰을 받아옵니다
+        y = self.classifier(x)
+        x = torch.cat((x, y), dim=1)
+        x = self.classifier2(x)
+        return x
