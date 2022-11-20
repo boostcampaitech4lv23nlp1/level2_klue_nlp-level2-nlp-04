61c5ab0c015f4df064b0f30185db0271d3a45e2e
YeRyeongLee <yyi06011@khu.ac.kr>
2022-11-20 16:36:08 +0000

    Feat: implement cm_and_error

--- Step/analysis.py
+++ Step/analysis.py
@@ -21,7 +21,7 @@ def get_predictions(args, conf):
         conf (_type_): conf
     
     Returns:
-        Tuple(torch.Tensor): test set에 대한 prediction, 전체 class에 대한 logit값
+        Tuple(list): test set에 대한 true값, prediction, 전체 class에 대한 logit값
     """
 
     with open('./dict_label_to_num.pkl', 'rb') as f:
@@ -42,11 +42,14 @@ def get_predictions(args, conf):
 
     predictions = list(i for i in torch.cat(predictions))
 
-    y_true = list(pd.read_csv(dataloader.test_path)['label'].apply(lambda x: label_to_num[x]))
+    df = pd.read_csv(dataloader.test_path)
+
+    x_text = list(sent+sub+obj for sent, sub, obj in zip(df['sentence'], df['subject_entity'], df['object_entity']))
+    y_true = list(df['label'].apply(lambda x: label_to_num[x]))
     y_pred = [np.argmax(logit, axis=-1).item() for logit in predictions]
     y_prob = [F.softmax(logit, dim=-1).tolist() for logit in predictions]
 
-    return y_true, y_pred, y_prob
+    return x_text, y_true, y_pred, y_prob
 
 
 def plot_confusion_matrix(args, conf):
@@ -61,7 +64,7 @@ def plot_confusion_matrix(args, conf):
         None: .png 확장자로 './analysis/cm_{model_name}.png' 으로 저장
     """
 
-    y_true, y_pred, _ = get_predictions(args, conf)
+    _, y_true, y_pred, _ = get_predictions(args, conf)
 
     dir_path = "./analysis"
     if not os.path.exists(dir_path):
@@ -79,7 +82,7 @@ def plot_confusion_matrix(args, conf):
     return 0
 
 
-def error_analysis(args, conf): # 아직!!
+def error_analysis(args, conf):
     """저장된 모델을 불러와서 test set에 대한 error analysis를 진행한 후
         csv형태로 저장하는 함수
 
@@ -90,8 +93,11 @@ def error_analysis(args, conf): # 아직!!
     Returns:
         None: .csv 확장자로 './analysis/error_{model_name}.csv'로 저장
     """
-    """
-    y_true, y_pred, y_prob = get_predictions(args, conf)
+
+    with open('./dict_num_to_label.pkl', 'rb') as f:
+        num_to_label = pickle.load(f)
+    
+    x_text, y_true, y_pred, y_prob = get_predictions(args, conf)
 
     dir_path = "./analysis"
     if not os.path.exists(dir_path):
@@ -100,11 +106,70 @@ def error_analysis(args, conf): # 아직!!
     key = "_".join([args.saved_model.split("/")[-2].split("_")[1], args.saved_model.split("/")[-1].split("-")[0]])
     file_name = 'error_' + key + '.csv'
 
-    max_sample = 100
+    ### CE 로스가 큰 순서대로 파일에 저장
 
-    ### TODO: max_sample개만큼 로스가 큰 순서대로 파일에 저장
+    texts = x_text
+    losses = []
+    trues = []
+    preds = []
+
+    loss = torch.nn.CrossEntropyLoss()
+
+    for true, pred, prob in zip(y_true, y_pred, y_prob):
+        losses.append(loss(torch.tensor([prob]), torch.tensor([pred])).item())
+        trues.append(num_to_label[true])
+        preds.append(num_to_label[pred])
 
     ###
-    """
+
+    df = pd.DataFrame({'text': texts, 'loss': losses, 'true': trues, 'pred': preds})
+    df = df.sort_values(by=['loss'], ascending=False)
+    df.to_csv(f'{dir_path}/{file_name}', index=False)
+    
     return 0
 
+
+def cm_and_error(args, conf):
+
+    with open('./dict_num_to_label.pkl', 'rb') as f:
+        num_to_label = pickle.load(f)
+
+    x_text, y_true, y_pred, y_prob = get_predictions(args, conf)
+
+    dir_path = "./analysis"
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+    
+    key = "_".join([args.saved_model.split("/")[-2].split("_")[1], args.saved_model.split("/")[-1].split("-")[0]])
+
+
+    ### confusion matrix
+
+    confmat = confusion_matrix(y_true, y_pred, normalize='true')
+    fig = sns.heatmap(confmat).get_figure()
+
+    fig.savefig(f'{dir_path}/cm_{key}.png')
+
+
+
+    ### CE 로스가 큰 순서대로 파일에 저장
+
+    texts = x_text
+    losses = []
+    trues = []
+    preds = []
+
+    loss = torch.nn.CrossEntropyLoss()
+
+    for true, pred, prob in zip(y_true, y_pred, y_prob):
+        losses.append(loss(torch.tensor([prob]), torch.tensor([pred])).item())
+        trues.append(num_to_label[true])
+        preds.append(num_to_label[pred])
+
+    ###
+
+    df = pd.DataFrame({'text': texts, 'loss': losses, 'true': trues, 'pred': preds})
+    df = df.sort_values(by=['loss'], ascending=False)
+    df.to_csv(f'{dir_path}/error_{key}.csv', index=False)
+
+    return 0
