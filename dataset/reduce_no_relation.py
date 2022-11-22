6d957577abdc6300d3e6d78d8aea6582ff4b6785
bongseok <wdg369@gmail.com>
2022-11-22 04:31:26 +0000

    FIX: Add train_path and argument

--- dataset/reduce_no_relation.py
+++ dataset/reduce_no_relation.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from omegaconf import OmegaConf
+import argparse
 
 
 def reduce_no_relation(delete_num, train_df):
@@ -9,13 +10,18 @@ def reduce_no_relation(delete_num, train_df):
     delete_df = train_df[train_df['label'] == "no_relation"].sample(n=delete_num, random_state=SEED)
     delete_list = list(delete_df['id'])
     result = train_df[train_df['id'].apply(lambda x: x not in delete_list) == True].reset_index(drop=True)
-    result.to_csv('train_reduce_no_relation.csv', index=False)
+    result.to_csv('train_reduce_no_relation2.csv', index=False)
 
     print(f'삭제 후 train_set 길이: {len(result)}')
 
 
 if __name__ == '__main__':
-    train_df = pd.read_csv('train/train.csv')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", "-c", type=str, default="base_config")
+    parser.add_argument("--delete_num", "-n", type=int, default=4000)
+    args, _ = parser.parse_known_args()
+    conf = OmegaConf.load(f"../Config/{args.config}.yaml")
+    train_df = pd.read_csv('../'+conf.path.train_path)
     # 삭제할 no_relation 개수 (EDA 결과 참고)
-    delete_num = 4000
+    delete_num = args.delete_num
     reduce_no_relation(delete_num, train_df)
