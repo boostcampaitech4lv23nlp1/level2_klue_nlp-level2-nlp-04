33b4428bc08865219074864758c72d66449b8dd7
54data <ehdvkf02@naver.com>
2022-11-23 09:16:21 +0000

    Fix: Add "LOC" in subject_entity_types

--- Instances/Dataloaders/dataloader.py
+++ Instances/Dataloaders/dataloader.py
@@ -45,7 +45,7 @@ class Dataloader(pl.LightningDataModule):
 
     def find_special_token(self, entity_marker_type):
         special_tokens = []
-        subject_entity_types = ["PER", "ORG"]
+        subject_entity_types = ["PER", "ORG", "LOC"]
         object_entity_types = ["PER", "ORG", "LOC", "DAT", "POH", "NOH"]
         if entity_marker_type == "typed_entity_marker":
             for i in subject_entity_types:
